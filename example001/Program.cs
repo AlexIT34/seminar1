77d14690e02d1bb1e4a6b39a03606607964fe7ec
Alex <asdsu@example.com>
2022-10-05 21:42:46 +0300

    Задачка да или нет

--- example001/Program.cs
+++ example001/Program.cs
@@ -1,3 +1,13 @@
-﻿Console.WriteLine("Введите целое число");
+﻿Console.WriteLine("Введите 1 число");
 int n = Convert.ToInt32(Console.ReadLine());
-Console.Write(n*n);
+Console.WriteLine("Введите 2 число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write(a*a);
+if (n == a*a)
+{
+    Console.WriteLine(" YES");
+}    
+else
+{
+    Console.WriteLine(" NO");
+}    
